8e8e56436c3750f26e543a2561528b397c7177e7
Lucien Dos Santos Farret <luciendsf@gmail.com>
2016-10-11 15:20:06 +0200

    added MgPair

---
+++ src/core/Mgpair.c
@@ -0,0 +1,27 @@
+
+#include "MgPair.h"
+
+
+MgStatus* MgPair_create(MgPair** pair,
+			MgObject* car,
+			MgObject* cdr) {
+  MgList* list;
+  MgStatus* status = MgList_create(&list);
+  if (status != Mg_ok) goto error;
+
+  status = MgList_push_front(&list, car);
+  if (status != Mg_ok) goto destroy_and_error;
+
+  /* replace cdr in list */
+  status = MgList_set_cdr(&list, cdr);
+  if (status != Mg_ok) goto destroy_and_error;
+
+  *pair = list;
+  
+  return Mg_ok;
+  
+ destroy_and_error:
+  MgList_destroy(list);
+ error:
+  return status;
+}
---
+++ src/core/include/MgPair.h
@@ -0,0 +1,8 @@
+
+#include "MgList.h"
+
+typedef MgPair MgList;
+
+MgStatus* MgPair_create(MgPair** pair,
+			MgObject* car,
+			MgObject* cdr);
