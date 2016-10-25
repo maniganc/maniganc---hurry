c29b8ba83c09ab6f0a93aeb1610a52fc1dc1729d
MHageH <Mohamed.HageHassan@yahoo.com>
2016-10-25 14:33:46 +0200

    Preliminary codes for procedures(primitives)

---
+++ src/procedure/procedures_macro.h
@@ -0,0 +1,22 @@
+#ifndef PROCEDURE_MACRO_H_
+#define PROCEDURE_MACRO_H_
+
+#include "MgProcedure.h"
+
+#define LINK(x, y) x##y
+#define MG_PROCEDURE_NAME(f) LINK(mg_procedure_,f)
+#define MG_PROCEDURE(f, identifier)                                     \
+  MgStatus* MG_PROCEDURE_NAME(f)(MgObject* arg,                         \
+                                         MgObject** output,             \
+                                         MgInterpreter* interpreter,    \
+                                         MgEnv* env)
+
+typedef struct Mg_procedure {
+  MgStatus* (*func)(MgObject* args,
+                    MgObject** output,
+                    MgObject* interpreter,
+                    MgEnv* env);
+  const char* name;
+}Mg_procedure;
+
+#endif /* PROCEDURE_MACRO_H_ */
