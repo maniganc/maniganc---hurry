e181b902b7ed5e3e13a7c4a166cd7599be893baa
Lucien Dos Santos Farret <luciendsf@gmail.com>
2016-11-29 15:38:13 +0100

    "and" procedure

---
+++ src/MgStd/conditional/and.c
@@ -0,0 +1,57 @@
+
+#include "buildin_procedures.h"
+
+#include "MgList.h"
+#include "MgIdentifier.h"
+#include "MgInterpreter.h"
+#include "MgBool.h"
+#include "MgObjectReference.h"
+
+
+MG_BUILDIN_PROCEDURE(and, "and") {
+  MgStatus* status;
+
+  /* check if there is no args */
+  if ((MgList*)arg == Mg_emptyList) {
+    *output = (MgObject*)MgBool_false;
+    return Mg_ok;
+  }
+
+  MgObject* returned_obj = (MgObject*)MgBool_false;
+
+  /* iterate over args */
+  MgObject* obj;
+  do {
+
+    /* get next object */
+    obj = MgList_get_car((MgList*)arg);
+
+    /* if it is the last object, we don't need to kwow its evaluation */
+    if (MgList_get_cdr((MgList*)arg) == (MgObject*)Mg_emptyList) {
+      return MgObjectReference_return_ref(obj,
+                                          (MgObjectReference**)output,
+                                          interpreter, env);
+    }
+
+    /* evaluate it */
+    MgObject* obj_eval;
+    MgStatus* s = MgObject_evaluate(obj, &obj_eval, interpreter, env);
+
+    /* check errors */
+    if (s != Mg_ok) {
+      return s;
+    }
+
+    /* skip false */
+    if (obj_eval != (MgObject*)MgBool_false) {
+      returned_obj = obj_eval;
+      break;
+    }
+
+    arg = MgList_get_cdr((MgList*)arg);
+  } while(1);
+
+  *output = (MgObject*)returned_obj;
+
+  return Mg_ok;
+}
