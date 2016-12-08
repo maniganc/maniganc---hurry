b2ac8cfaa82ba2856c0efba1c51d02cc3623eae5
Lucien Dos Santos Farret <luciendsf@gmail.com>
2016-12-08 14:44:25 +0100

    cons code&tests

--- other-test/MgStd/object/MgList.scm
+++ other-test/MgStd/object/MgList.scm
@@ -22,13 +22,16 @@
 ;; command expect failure
 ;; feed
 (null?)
+;; pause feed
 ;; run command
 
 ;; description: null? too much args
 ;; command expect failure
 ;; feed
 (null? 1 2)
+;; pause feed
 ;; run command
+
 
 
 ;; description: list?
@@ -51,13 +54,16 @@
 ;; command expect failure
 ;; feed
 (list?)
+;; pause feed
 ;; run command
 
 ;; description: list? too much args
 ;; command expect failure
 ;; feed
 (list? '() '(1))
+;; pause feed
 ;; run command
+
 
 
 ;; description: pair?
@@ -80,12 +86,14 @@
 ;; command expect failure
 ;; feed
 (pair?)
+;; pause feed
 ;; run command
 
 ;; description: pair? too much args
 ;; command expect failure
 ;; feed
 (pair? '(1 . 2) '("a" . (1)))
+;; pause feed
 ;; run command
 
 
@@ -113,22 +121,65 @@
 ;; command expect failure
 ;; feed
 (list->string)
+;; pause feed
 ;; run command
 
 ;; description: list->string too much args
 ;; command expect failure
 ;; feed
 (list->string '(#\a) '())
+;; pause feed
 ;; run command
 
 ;; description: list->string improper list
 ;; command expect failure
 ;; feed
 (list->string '(#\a . #\b))
+;; pause feed
 ;; run command
 
 ;; description: list->string not a list
 ;; command expect failure
 ;; feed
 (list->string 'symbol)
+;; pause feed
+;; run command
+
+
+
+;; description: cons
+;; command expect success
+;; feed
+(write (cons 1 2))
+(write
+ (cons
+  (cons 2 '())
+  '()))
+(define va #\y)
+(define vb #\z)
+(write (cons va vb))
+;; check
+(1 . 2)
+((2))
+(#\y . #\z)
+;; pause check
+;; run command
+
+;; description: cons not enough args
+;; command expect failure
+;; feed
+(cons)
+;; pause feed
+;; run command
+
+;; description: cons not enough args 2
+;; feed
+(cons 1)
+;; pause feed
+;; run command
+
+;; description: cons too much args
+;; feed
+(cons 1 2 3)
+;; pause feed
 ;; run command
--- src/MgStd/object/MgList.c
+++ src/MgStd/object/MgList.c
@@ -9,14 +9,15 @@
 #include "MgObjectReference.h"
 #include "MgString.h"
 #include "MgChar.h"
-
-static const MgStatus error_params = {
-  .message = "null? require one argument"
-};
+#include "MgPair.h"
 
 /* predicates */
 
 MG_BUILDIN_PROCEDURE(is_null, "null?") {
+
+  static const MgStatus error_params = {
+    .message = "null? require one argument"
+  };
 
   /* check for arg */
   if ((MgList*)arg == Mg_emptyList) {
@@ -48,12 +49,11 @@ MG_BUILDIN_PROCEDURE(is_null, "null?") {
 }
 
 
-static const MgStatus error_params_list_p = {
-  .message = "list? require one argument"
-};
-
-
 MG_BUILDIN_PROCEDURE(is_list, "list?") {
+
+  static const MgStatus error_params_list_p = {
+    .message = "list? require one argument"
+  };
 
   /* check for arg */
   if ((MgList*)arg == Mg_emptyList) {
@@ -84,13 +84,11 @@ MG_BUILDIN_PROCEDURE(is_list, "list?") {
   return Mg_ok;
 }
 
-
-static const MgStatus error_params_pair_p = {
-  .message = "pair? require one argument"
-};
-
-
 MG_BUILDIN_PROCEDURE(is_pair, "pair?") {
+
+  static const MgStatus error_params_pair_p = {
+    .message = "pair? require one argument"
+  };
 
   /* check for arg */
   if ((MgList*)arg == Mg_emptyList) {
@@ -125,11 +123,12 @@ MG_BUILDIN_PROCEDURE(is_pair, "pair?") {
 /* conversions */
 
 
-static const MgStatus error_params_list_to_string = {
-  .message = "list->string require one proper list of char"
-};
-
 MG_BUILDIN_PROCEDURE(list_to_string, "list->string") {
+
+  static const MgStatus error_params_list_to_string = {
+    .message = "list->string require one proper list of char"
+  };
+
   MgStatus* s = &error_params_list_to_string;
 
   /* one arg only */
@@ -137,13 +136,15 @@ MG_BUILDIN_PROCEDURE(list_to_string, "list->string") {
   if ((MgList*)MgList_get_cdr((MgList*)arg) != Mg_emptyList) goto error;
 
   MgObject* list_ref = MgList_get_car((MgList*)arg);
-  
+
   MgList* list_eval;
-  MgObject_evaluate(list_ref, (MgObject**)&list_eval, interpreter, env);
+  s = MgObject_evaluate(list_ref, (MgObject**)&list_eval, interpreter, env);
+  if ( s != Mg_ok ) goto error;
 
   MgObject_add_reference((MgObject*)list_eval);
-  
+
   /* check if it is really a list */
+  s = &error_params_list_to_string;
   if (!Mg_is_a_list((MgObject*)list_eval)) {
     MgObject_drop_reference((MgObject*)list_eval);
     goto error;
@@ -157,10 +158,10 @@ MG_BUILDIN_PROCEDURE(list_to_string, "list->string") {
   MgList* sublist = list_eval;
   while(sublist != Mg_emptyList) {
     if (!Mg_is_a_list((MgObject*)sublist)) goto drop_eval_and_other_and_error;
-    
+
     /* get next object */
     MgObject* obj = MgList_get_car((MgList*)sublist);
-    
+
     /* check if it's a char */
     if (!Mg_is_a_char(obj)) goto drop_eval_and_other_and_error;
 
@@ -185,13 +186,60 @@ MG_BUILDIN_PROCEDURE(list_to_string, "list->string") {
   vector_char_deinit(&str);
 
   *output = (MgObject*)mgstring;
-  
+
   return Mg_ok;
 
   drop_eval_and_other_and_error:
   MgObject_drop_reference((MgObject*)list_eval);
   free_vector_and_error:
   vector_char_deinit(&str);
+  error:
+  return s;
+}
+
+MG_BUILDIN_PROCEDURE(cons, "cons") {
+
+  static const MgStatus error_params = {
+    .message = "cons require two arguments"
+  };
+
+  MgStatus* s;
+
+  /* require two args */
+  s = &error_params;
+  if ( arg == (MgObject*)Mg_emptyList ) goto error;
+  MgObject* arg1 = MgList_get_car((MgList*)arg);
+
+  arg = MgList_get_cdr((MgList*)arg);
+  if ( arg == (MgObject*)Mg_emptyList ) goto error;
+  MgObject* arg2 = MgList_get_car((MgList*)arg);
+
+  arg = MgList_get_cdr((MgList*)arg);
+  if ( arg != (MgObject*)Mg_emptyList ) goto error;
+
+  /* evaluate args */
+  MgObject* arg1_eval;
+  s = MgObject_evaluate(arg1, &arg1_eval, interpreter, env);
+  if ( s != Mg_ok ) goto error;
+  MgObject_add_reference(arg1_eval);
+
+  MgObject* arg2_eval;
+  s = MgObject_evaluate(arg2, &arg2_eval, interpreter, env);
+  if ( s != Mg_ok ) goto drop_arg1_and_error;
+  MgObject_add_reference(arg2_eval);
+
+  /* return pair */
+  MgPair* pair;
+  s = MgPair_create(&pair, arg1_eval, arg2_eval);
+  if ( s != Mg_ok ) goto drop_arg2_and_error;
+
+  *output = (MgObject*)pair;
+  return Mg_ok;
+
+  drop_arg2_and_error:
+  MgObject_drop_reference(arg2_eval);
+  drop_arg1_and_error:
+  MgObject_drop_reference(arg1_eval);
   error:
   return s;
 }
